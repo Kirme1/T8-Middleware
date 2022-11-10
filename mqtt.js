1dfb6c89506535f4ca5e82010873df0b42d6815f
Yasamin <gusfazya@student.gu.se>
2022-11-10 11:52:33 +0100

    Add mqtt.js

---
+++ mqtt.js
@@ -0,0 +1,34 @@
+const mqtt = require('mqtt')
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+const options = {
+  // Clean session
+  clean: true,
+  connectTimeout: 4000,
+ 
+}
+const connectUrl = `mqtt://localhost:${port}`
+
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'emqx',
+  password: 'public',
+  reconnectPeriod: 1000,
+})
+const topic = '/nodejs/mqtt'
+client.on('connect', () => {
+  console.log('Connected')
+  client.subscribe([topic], () => {
+    console.log(`Subscribe to topic '${topic}'`)
+  })
+  client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+    if (error) {
+      console.error(error)
+    }
+  })
+})
+client.on('message', (topic, payload) => {
+  console.log('Received Message:', topic, payload.toString())
+})
